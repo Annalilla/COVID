e1645cba7a287279fce981f25514a7c44688792c
Annalilla <harman.anna@gmail.com>
2020-12-28 10:07:15 +0100

    Update near_weather_stations.R

--- helpers/near_weather_stations.R
+++ helpers/near_weather_stations.R
@@ -5,7 +5,9 @@ station_data <- ghcnd_stations()
 
 near_stations <- NULL
 
+# In some cases selected manual
 for(i in 1:nrow(capitals)){
-  station_df <- get_near_stations(capitals$Capital[i], capitals$Latitudes[i], capitals$Longitudes[i])
+  city <- data.frame(id = capitals$Capital[i], latitude = as.numeric(capitals$Latitudes[i]), longitude = as.numeric(capitals$Longitudes[i]))
+  station_df <- meteo_nearby_stations(lat_lon_df = city, station_data = station_data, radius = 20, var = c("TAVG"), year_min = 2020, year_max = 2020)
   near_stations <- c(near_stations, station_df$id[1])
 }
