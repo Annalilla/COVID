141674ffefad75226520094c35de01d6766ae32c
Annalilla <harman.anna@gmail.com>
2021-01-07 17:12:12 +0100

    Update Save_data.R
    Change in filename

--- data_collection/Save_data.R
+++ data_collection/Save_data.R
@@ -1,9 +1,10 @@
 save_data <- function(data, datname, dirname = NA, archieve = FALSE)
 {
   filename <- paste(getwd(), "/", dirname, "/", datname, ".csv", sep = "")
+  attach <- str_replace_all(str_extract_all(Sys.time(), ".+ \\d+:\\d+:\\d+"), "[ :]", "_")
   if(archieve == TRUE)
   {
-    archived_file <- paste(getwd(), "/", dirname, "/", datname, "_", Sys.Date(), ".csv", sep = "")
+    archived_file <- paste(getwd(), "/", dirname, "/", datname, "_", attach, ".csv", sep = "")
     if(file.exists(filename)) file.rename(filename, archived_file)
   }
   write.csv(data, filename, row.names = FALSE)
