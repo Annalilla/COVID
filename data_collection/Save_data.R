5936919d5281006d96bdf6c1aa63b69b341ff1ab
Annalilla <harman.anna@gmail.com>
2021-01-17 17:10:55 +0100

    Update Save_data.R
    Save data to googlesheets

--- data_collection/Save_data.R
+++ data_collection/Save_data.R
@@ -9,3 +9,30 @@ save_data <- function(data, datname, dirname = NA, archieve = FALSE)
   }
   write.csv(data, filename, row.names = FALSE)
 }
+
+save_data_online <- function(data, datname, archieve = FALSE)
+{
+  # File for authentication available
+  if(file.exists(".secrets/dbea2094459b0986454785bfcd2781b5_covid.data.storage@gmail.com")){
+    if(!("googlesheets4" %in% (.packages()))) library(googlesheets4)
+    if(!gs4_has_token()){
+      # google sheets authentication
+      gs4_auth(
+        cache = ".secrets",
+        email = "covid.data.storage@gmail.com"
+      )
+    }
+  
+    covid_sheet <- "https://docs.google.com/spreadsheets/d/1K6jzpSKJHVQBVyRfAtVhkOcgepGEnrTVK-Ifnw85_vM/edit#gid=0"
+    if(archieve == TRUE)
+    {
+      attach <- str_replace_all(str_extract_all(Sys.time(), ".+ \\d+:\\d+:\\d+"), "[ :]", "_")
+      if(datname %in% sheet_names(covid_sheet)){
+        sheet_rename(covid_sheet, sheet = datname, new_name = paste(datname, attach, sep = "_"))
+      }
+    }
+    sheet_write(tdata, ss = covid_sheet, sheet = datname)
+  }else{
+      cat("Save data to googlesheets was unsuccesfull - File for authentication not available")
+  }
+}
