ad41ded0886dd43c2a0305e2a2862fabc993a629
Annalilla <harman.anna@gmail.com>
2021-01-17 17:12:06 +0100

    Create Get_data.R
    Read data - local or from googlesheets

---
+++ main/Get_data.R
@@ -0,0 +1,30 @@
+get_data <- function(datname, dirname = NA, local = FALSE)
+{
+  if(local == TRUE){
+    fileurl <- paste(dirname, "/", datname, ".csv", sep = "")
+    if(file.exists(fileurl)){
+      read.csv(fileurl)
+    }else{
+      cat("File not Found")
+    }
+  }else if(local == FALSE){
+    # File for authentication available
+    if(file.exists(".secrets/dbea2094459b0986454785bfcd2781b5_covid.data.storage@gmail.com")){
+      if(!("googlesheets4" %in% (.packages()))) library(googlesheets4)
+      if(!gs4_has_token()){
+        # google sheets authentication
+        gs4_auth(
+          cache = ".secrets",
+          email = "covid.data.storage@gmail.com"
+        )
+      }
+      
+      covid_sheet <- "https://docs.google.com/spreadsheets/d/1K6jzpSKJHVQBVyRfAtVhkOcgepGEnrTVK-Ifnw85_vM/edit#gid=0"
+      if(datname %in% sheet_names(covid_sheet)){
+        range_speedread(covid_sheet, sheet = datname)
+      }
+    }else{
+        cat("Read data from googlesheets was unsuccesfull - File for authentication not available")
+    }
+  }
+}
