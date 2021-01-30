75923a56e72ea93cd7b5486301e695168e23f1f4
Annalilla <harman.anna@gmail.com>
2021-01-30 08:23:16 +0100

    Update Create_database.R

--- data_collection/Create_database.R
+++ data_collection/Create_database.R
@@ -2,5 +2,10 @@ source("data_collection/Save_data.R")
 source("data_collection/Collect_data.R")
 source("data_collection/Merge_data.R")
 
+# Save local
 save_data(country_char, datname = "country_char", dirname = "data", archieve = TRUE)
 save_data(tdata, datname = "tdata", dirname = "data", archieve = TRUE)
+
+# Save online
+save_data_online(country_char, datname = "country_char", archieve = TRUE)
+save_data_online(tdata, datname = "tdata", archieve = TRUE)
