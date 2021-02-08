8c0d25f277be3ac4ea52e3bc5577150fb9d38ad9
Annalilla <harman.anna@gmail.com>
2021-02-08 20:02:36 +0100

    Update Data_revision_functions.R

--- functions/Data_revision_functions.R
+++ functions/Data_revision_functions.R
@@ -15,8 +15,8 @@ show_last_differences <- function(n){
 
 show_all_differences_in_one_var <- function(varname){
   act_diff <- differences[[which(names(differences) == varname)]]
-  diffs_in_record <- cbind(to_compare[act_diff, c(which(colnames(to_compare) %in% c("country", "date", varname)))],
-                           old_tdata[act_diff, which(colnames(old_tdata) == varname)])
+  diffs_in_record <- cbind(common_vars[act_diff, c(which(colnames(common_vars) %in% c("country", "date", varname)))],
+                           old_tdata_common[act_diff, which(colnames(old_tdata_common) == varname)])
   colnames(diffs_in_record)[ncol(diffs_in_record)] <- paste("old value of ", varname, sep = "")
   return(diffs_in_record)
 }
@@ -25,28 +25,35 @@ show_new_vars <- function(){
   colnames(to_compare)[-which(colnames(to_compare) %in% colnames(old_tdata))]
 }
 
+show_extinct_vars <- function(){
+  colnames(old_tdata)[-which(colnames(old_tdata) %in% colnames(to_compare))]
+}
+
 # Extract differences
 find_different_records <- function(diffs){
-  colnum <- unlist(lapply(seq_along(diffs), function(i){which(colnames(to_compare) == names(diffs)[i])}))
-  res <- lapply(seq_along(colnum), function(x) differences_in_record(nx = x, listx = diffs, colnumx = colnum))
+  colnum_common_vars <- unlist(lapply(seq_along(diffs), function(i){which(colnames(common_vars) == names(diffs)[i])}))
+  colnum_old_tdata <- unlist(lapply(seq_along(diffs), function(i){which(colnames(old_tdata_common) == names(diffs)[i])}))
+  
+  res <- lapply(seq_along(colnum_common_vars), function(x) differences_in_record(nx = x, listx = diffs, colnum_commonx = colnum_common_vars,
+                                                                     colnum_oldx = colnum_old_tdata))
   return(res)
 }
 
-differences_in_record <- function(nx, listx, colnumx){
-  diffs_in_record <- cbind(to_compare[listx[[nx]], c(which(colnames(to_compare) %in% c("country", "date")), colnumx[[nx]])],
-                           old_tdata[listx[[nx]], colnumx[[nx]]])
-  colnames(diffs_in_record)[ncol(diffs_in_record)] <- paste("old value of ", colnames(to_compare)[colnumx[[nx]]], sep = "")
+differences_in_record <- function(nx, listx, colnum_commonx, colnum_oldx){
+  diffs_in_record <- cbind(common_vars[listx[[nx]], c(which(colnames(common_vars) %in% c("country", "date")), colnum_commonx[[nx]])],
+                           old_tdata_common[listx[[nx]], colnum_oldx[[nx]]])
+  colnames(diffs_in_record)[ncol(diffs_in_record)] <- paste("old value of ", colnames(common_vars)[colnum_commonx[[nx]]], sep = "")
   return(diffs_in_record)
 }
 
 # Update old records
 update_var <- function(varname, records = "all"){
-  old_dat <- old_tdata
+  old_dat <- old_tdata_common
   if(records[1] == "all"){
     act_diff <- differences[[which(names(differences) == varname)]]
-    old_dat[act_diff, which(colnames(old_dat) == varname)] <- tdata[act_diff, which(colnames(tdata) == varname)]
+    old_dat[act_diff, which(colnames(old_dat) == varname)] <- common_vars[act_diff, which(colnames(common_vars) == varname)]
   }else{
-    old_dat[records, which(colnames(old_dat) == varname)] <- tdata[records, which(colnames(tdata) == varname)]
+    old_dat[records, which(colnames(old_dat) == varname)] <- common_vars[records, which(colnames(common_vars) == varname)]
   }
   return(old_dat)
 }
@@ -57,5 +64,3 @@ add_new_var <- function(varname){
   colnames(old_tdata)[ncol(old_tdata)] <- varname
   return(old_tdata)
 }
-
-
