d70911bf037432a57b5eb0e467076d45aefc6b74
Annalilla <harman.anna@gmail.com>
2021-03-05 13:47:35 +0100

    Update Call_shiny.R

--- shinydashboard/Call_shiny.R
+++ shinydashboard/Call_shiny.R
@@ -1,6 +1,6 @@
 library(shinydashboard)
 
 source("functions/Shiny_prep_functions.R")
-source("shinydashboard/Shiny_data_prep.r")
+source("shinydashboard/Shiny_data_prep.R")
 
 runApp("shinydashboard")
