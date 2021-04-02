85a252e635755a691de88f4c74fa5177f0991eef
Annalilla <harman.anna@gmail.com>
2021-04-02 07:44:42 +0200

    Add files via upload

---
+++ shinydashboard/Shiny_prep_and_functions.R
@@ -0,0 +1,160 @@
+country_list <- readRDS("dat/country_list.RDS")
+rest_names <- readRDS("dat/rest_names.RDS")
+y_limit_list <- readRDS("dat/y_limit_list.RDS")
+rest_prev <- readRDS("dat/rest_prev.RDS")
+sel_rest_country <- readRDS("dat/sel_rest_country.RDS")
+
+# Functions
+smooth_or_not <- function(to_smooth, cvar, min_date, max_date){
+  df <- country_list[[cvar]][which((country_list[[cvar]]$date >= min_date) & (country_list[[cvar]]$date <= max_date)),]
+  if(to_smooth == TRUE){
+    df <- df[,c(which(colnames(df) %in% c("date", "smoothed_cases", "smoothed_tavg")))]
+  }else{
+    df <- df[,c(which(colnames(df) %in% c("date", "cases_new", "tavg")))]
+  }
+  colnames(df) <- c("date", "cases", "tavg")
+  return(df)
+}
+
+y_limits <- function(lim_list, to_smooth, temp){
+  # Maximum
+  if(to_smooth == TRUE){
+    max_limit <- lim_list[which(colnames(lim_list) == "y_limit_smo")]
+  }else{
+    max_limit <- lim_list[which(colnames(lim_list) == "y_limit_abs")]
+  }
+  # Minimum
+  if(temp == TRUE){
+    if(to_smooth == TRUE){
+      min_limit <- min(0, lim_list[which(colnames(lim_list) == "y_min_smo_temp")])
+    }else{
+      min_limit <- min(0, lim_list[which(colnames(lim_list) == "y_min_abs_temp")])
+    }
+  }
+  else{
+    min_limit <- 0
+  }
+  limits <- c(min_limit, max_limit)
+  return(limits)
+}
+
+ci_from_se <- function(var, se){
+  res <- cbind((var - (qnorm(0.995) * se)), (var + (qnorm(0.995) * se)))
+  return(res)
+}
+
+fb_smooth_or_not <- function(to_smooth, cvar, min_date, max_date, max_y){
+  df <- country_list[[cvar]][which((country_list[[cvar]]$date >= min_date) & (country_list[[cvar]]$date <= max_date)),]
+  if(to_smooth == TRUE){
+    df <- cbind(df[,c("fb_data.smoothed_cli", "fb_data.smoothed_mc", "fb_data.smoothed_dc")],
+                ci_from_se(df$fb_data.smoothed_cli, df$fb_data.smoothed_cli_se),
+                ci_from_se(df$fb_data.smoothed_mc, df$fb_data.smoothed_mc_se),
+                ci_from_se(df$fb_data.smoothed_dc, df$fb_data.smoothed_dc_se))
+  }else{
+    df <- cbind(df[,c(which(colnames(df) %in% c("fb_data.percent_cli", "fb_data.percent_mc", "fb_data.percent_dc")))],
+                ci_from_se(df$fb_data.percent_cli, df$fb_data.cli_se),
+                ci_from_se(df$fb_data.percent_mc, df$fb_data.mc_se),
+                ci_from_se(df$fb_data.percent_dc, df$fb_data.dc_se))
+  }
+  colnames(df) <- c("cli", "mc", "dc", "cli_ci_l", "cli_ci_u", "mc_ci_l", "mc_ci_u", "dc_ci_l", "dc_ci_u")
+  df <- apply(df, 2, function(x) x * max_y)
+  return(df)
+}
+
+# Vaccination: people vaccinated or people fully vaccinated?
+vacc_smooth_or_not <- function(to_smooth, cvar, min_date, max_date, max_y){
+  df <- country_list[[cvar]][which((country_list[[cvar]]$date >= min_date) & (country_list[[cvar]]$date <= max_date)),]
+  if(to_smooth == TRUE){
+    df <- df[,c("date", "smoothed_people_vaccinated_per_hundred")]
+  }else{
+    df <- df[,c("date", "people_vaccinated_per_hundred")]
+  }
+  colnames(df) <- c("date", "vaccination")
+  df$vaccination <- df$vaccination * max_y/100
+  # Setting NAs before the first nonnull value (no vaccination before)
+  if(length(which(df$vaccination > 0)) > 0){
+    df$vaccination[1:(which(df$vaccination > 0)[1] - 1)] <- NA
+  }else{
+    df$vaccination <- rep(NA, nrow(df))
+  }
+  return(df)
+}
+
+number_of_cases <- function(cvar, min_date, max_date){
+  tmp <- country_list[[cvar]][which((country_list[[cvar]]$date >= min_date) & (country_list[[cvar]]$date <= max_date)),]
+  no_cases <- sum(tmp$cases_new, na.rm = TRUE)
+  no_deaths <- sum(tmp$deaths_new, na.rm = TRUE)
+  no_recovered <- sum(tmp$recovered_new, na.rm = TRUE)
+  no_vacc <- sum(tmp$new_vaccinations, na.rm = TRUE)
+  number_of_cases <- c(no_cases, no_deaths, no_recovered, no_vacc)
+}
+
+add_lead <- function(dat, lead_to_add){
+  dat$cases <- lead(dat$cases, lead_to_add)
+  dat <- dat[1:(nrow(dat) - lead_to_add),]
+  return(dat)
+}
+
+get_x_for_rest <- function(rest, data){
+  k <- data
+  if(rest %in% colnames(k)){
+    colnames(k) <- c(colnames(k)[1:(ncol(k)/2)], paste(colnames(k)[1:(ncol(k)/2)], "prev", sep = "_"))
+    k$x_min <- as.Date(NA)
+    k$x_max <- as.Date(NA)
+    
+    rest_prev <- paste(rest, "prev", sep = "_")
+    k <- k[, which(colnames(k) %in% c("date", rest, "date_prev", rest_prev, "x_min", "x_max"))]
+    k$x_min[which(k[,which(colnames(k) == rest_prev)] == 0 & k[,which(colnames(k) == rest)] == 1)] <-
+      k$date[which(k[,which(colnames(k) == rest_prev)] == 0 & k[,which(colnames(k) == rest)] == 1)]
+    k$x_max[which(k[,which(colnames(k) == rest_prev)] == 1 & k[,which(colnames(k) == rest)] == 0)] <-
+      k$date_prev[which(k[,which(colnames(k) == rest_prev)] == 1 & k[,which(colnames(k) == rest)] == 0)]
+    if(k[nrow(k),which(colnames(k) == rest)] == 1){
+      k$x_max[nrow(k)] <- k$date[nrow(k)]
+    }
+    k_coord <- data.frame("x_min" = k$x_min[which(!is.na(k$x_min))], "x_max" = k$x_max[which(!is.na(k$x_max))])
+    k_coord <- cbind("restriction" = rep(rest, nrow(k_coord)), k_coord)
+  }else{
+    k_coord <- cbind("restriction" = rest, "x_min" = 1999, "x_max" = NA)
+  }
+  return(k_coord)
+}
+
+get_x_for_country <- function(country){
+  act_all_rest <- sel_rest_country[[which(names(sel_rest_country) == country)]]
+  act_x <- lapply(act_all_rest, function(x) get_x_for_rest(x, rest_prev[[which(names(rest_prev) == country)]]))
+  names(act_x) <- act_all_rest
+  act_x
+}
+
+get_coord_for_country <- function(country, rest_list, max_y){
+  dat <- rest_prev[[which(names(rest_prev) == country)]]
+  # X coordinates
+  act_x <- lapply(rest_list, function(x) get_x_for_rest(x, dat))
+  names(act_x) <- rest_list
+  
+  # Y coordinates
+  coord <- seq(0, max_y, len = length(rest_list) + 1)
+  if(length(rest_list) == 1){
+    coord <- as.data.frame(cbind(rest_list, t(coord)))
+  }else{
+    coord <- c(coord[1], rep(coord[2:(length(coord) - 1)], each = 2), coord[length(coord)])
+    coord <- t(do.call(rbind, (split(coord, c(1,length(rest_list))))))
+    coord <- as.data.frame(cbind(rest_list, coord))
+  }
+  colnames(coord) <- c("restriction", "y_min", "y_max")
+  coord[, c("y_min", "y_max")] <- lapply(coord[, c("y_min", "y_max")], as.numeric)
+  
+  # All coordinates into a dataframe
+  rest_coord <- lapply(rest_list, function(x) act_x[[x]] <- cbind(act_x[[x]], coord[coord$restriction == x, c("y_min", "y_max")], row.names = NULL))
+  names(rest_coord) <- rest_list
+  rest_coord <- do.call(rbind, rest_coord)
+  
+  # Colorlist
+  colors <- brewer.pal(n = 8, name = 'Pastel2')
+  col_list <- cbind(rest_list, colors[1:length(rest_list)])
+  colnames(col_list) <- c("restriction", "color")
+  rest_coord <- merge(rest_coord, col_list, by = "restriction", all.x = TRUE)
+  
+  rest_coord
+}
+
---
+++ shinydashboard/Shiny_vis_functions.R
@@ -0,0 +1,148 @@
+# Contains functions to respond interactively to the queries in the shiny application.
+
+exp_subtitle <- function(ilead, max_lead){
+  if(!is.na(ilead) & ilead != "")
+  {
+    lead_bad <- is.na(as.numeric(ilead))
+    if(!lead_bad){
+      input_lead <- as.numeric(ilead)
+      if(input_lead > 0 & input_lead < nrow(max_lead)){
+        subt <- paste("Number of infections with", ilead, "days lead", sep = " ")
+      }else if(input_lead < 0){
+        subt <- paste("Lead has to be positive")
+      }else if(input_lead == 0){
+        subt <- paste("Number of infections")
+      }else{
+        subt <- paste("Lead is too big")
+      }
+    }else{
+      subt <- paste("Lead has to be numeric")
+    }
+  }else{
+    subt <- paste("Number of infections")
+  }
+  return(subt)
+}
+
+exp_plot_base <- function(dat, y_min, y_max, mc, dc, vacc){
+  p <- ggplot(dat) +
+        geom_line(aes(x = date, y = cases), size = 0.8) +
+        xlab("Date") +
+        ylab("Cases") +
+        theme_minimal() +
+        theme(axis.text.x=element_text(angle=60, hjust=1),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              plot.margin = unit(c(1,1,0,1), "lines")) +
+        coord_cartesian(xlim = c(min(dat$date), max(dat$date)), clip = 'off')
+  # Second Y axis labels
+  if(mc == TRUE | dc == TRUE | (vacc == TRUE & length(which(dat$vaccination > 0)) > 0)){
+  varnames <- NULL
+  if(mc == TRUE) varnames <- c(varnames, "Mask Coverage")
+  if(dc == TRUE) varnames <- c(varnames, "Direct Contact")
+  if((vacc == TRUE & length(which(dat$vaccination > 0)) > 0)) varnames <- c(varnames, "Vaccination")
+  axis_title <- paste(paste(varnames, collapse = " & "), "%", sep = " ")
+  p <- p +
+    scale_y_continuous(limits = c(y_min, y_max),
+                       sec.axis = sec_axis(~./y_max, name = axis_title,
+                                           labels = function(x) { paste0(round(x * 100, 0), "%")}))
+  }
+  else{
+    p <- p +
+      scale_y_continuous(limits = c(y_min, y_max))
+  }
+  return(p)
+}
+
+exp_plot_add_temp <- function(plot, temp, dat){
+  if(temp == TRUE){ 
+    x_dist <- as.numeric(round((max(dat$date, na.rm = TRUE) - min(dat$date, na.rm = TRUE))/25, 0))
+    tcol <- brewer.pal(n = 3, name = 'Set1')[1]
+    multi <- max(dat$cases, na.rm = TRUE)/max(dat$tavg, na.rm = TRUE)
+    labpx1 <- (dat[which.max(dat$tavg), "date"] + x_dist)
+    labpy1 <- (multi * dat[1, "tavg"] + multi)
+    labt1 <- paste(round(dat[1, "tavg"], 1), "°C", sep = "")
+    pointx1 <- dat[1, "date"]
+    pointy1 <- (multi * dat[1, "tavg"])
+    labpx <- (dat[which.max(dat$tavg), "date"] + x_dist)
+    labpy <- (multi * dat[which.max(dat$tavg), "tavg"] + multi)
+    labt <- paste(round(dat[which.max(dat$tavg), "tavg"], 1), "°C", sep = "")
+    pointx <- dat[which.max(dat$tavg), "date"]
+    pointy <- (multi * dat[which.max(dat$tavg), "tavg"])
+    labpxmin <- (dat[which.min(dat$tavg), "date"] + x_dist)
+    labpymin <- (multi * dat[which.min(dat$tavg), "tavg"])
+    labtmin <- paste(round(dat[which.min(dat$tavg), "tavg"], 1), "°C", sep = "")
+    pointxmin <- dat[which.min(dat$tavg), "date"]
+    pointymin <- (multi * dat[which.min(dat$tavg), "tavg"])
+    
+    plot <- plot +
+      geom_line(aes(x = date, y = (multi * tavg)), color = tcol, size = 0.8, alpha=0.7) +
+      geom_point(aes(x = pointx, y = pointy), color = tcol, size = 2, alpha = 0.7) +
+      geom_point(aes(x = pointxmin, y = pointymin), color = tcol, size = 2, alpha = 0.7) +
+      annotate("text", x = labpxmin, y = labpymin, label = labtmin, color = tcol) +
+      annotate("text", x = labpx, y = labpy, label = labt, color = tcol)
+  }
+  return(plot)
+}
+
+exp_plot_add_fb_vacc <- function(plot, mc, dc, vacc, dat){
+  if(mc == TRUE | dc == TRUE | (vacc == TRUE & length(which(dat$vaccination > 0)) > 0)){
+    select <- NULL
+    varnames <- NULL
+    if(mc == TRUE){
+      select <- c(select, "mc")
+      varnames <- c(varnames, "Mask Coverage")
+    }
+    if(dc == TRUE){
+      select <- c(select, "dc")
+      varnames <- c(varnames, "Direct Contact")
+    }
+    if(vacc == TRUE & length(which(dat$vaccination > 0)) > 0){
+      select <- c(select, "vaccination")
+      varnames <- c(varnames, "Vaccination")
+    }
+    dat_l <- dat[,c("date", colnames(dat)[which(colnames(dat) %in% select)])]
+    colnames(dat_l) <- c("date", varnames)
+    df <- melt(dat_l, id.vars = 1)
+    plot <- plot + 
+      geom_line(data = df, aes(x = date, y = value, colour = variable), size = 0.8) +
+      theme(legend.position = "bottom",
+            legend.title = element_blank()) +
+      scale_color_brewer(palette = "Dark2")
+    
+    if(mc == TRUE)
+    {
+      plot <- plot +
+        geom_ribbon(data = dat, aes(x = date, ymin = mc_ci_l, ymax = mc_ci_u), fill ="grey", alpha=0.3)
+    }
+    if(dc == TRUE)
+    {
+      plot <- plot +
+        geom_ribbon(data = dat, aes(x = date, ymin = dc_ci_l, ymax = dc_ci_u), fill = "grey", alpha=0.3)
+    }
+  }
+  return(plot)
+}
+
+exp_display_plot <- function(plot, rest, plotrest, plotdata, restlabels, mc, dc){
+  if(length(rest) > 0){
+    # x axis for restriction labels
+    if(mc == FALSE & dc == FALSE) {rest_x <- rep(max(plotdata$date) + 5, nrow(restlabels))}
+    else{rest_x <- rep(max(plotdata$date) + 100, nrow(restlabels))} 
+    if(is.Date(plotrest$x_min) & is.Date(plotrest$x_max)){
+      plotrest$x_max[which(plotrest$x_max > max(plotdata$date, na.rm = TRUE))] <- max(plotdata$date, na.rm = TRUE)
+      plotrest$x_min[which(plotrest$x_min < min(plotdata$date, na.rm = TRUE))] <- min(plotdata$date, na.rm = TRUE)
+      plotrest$x_min[which(plotrest$x_min > max(plotdata$date, na.rm = TRUE))] <- NA
+      plotrest$x_max[which(plotrest$x_min > max(plotdata$date, na.rm = TRUE))] <- NA
+      plotrest$x_min[which(plotrest$x_max < min(plotdata$date, na.rm = TRUE))] <- NA
+      plotrest$x_max[which(plotrest$x_max < min(plotdata$date, na.rm = TRUE))] <- NA
+      # Adding restrictions to plot
+      plot <- plot +
+        theme(plot.margin = unit(c(1,12,0,1), "lines")) +
+        geom_rect(data = plotrest, aes(xmin = x_min, xmax = x_max, ymin = y_min, ymax = y_max, fill = color), show.legend = FALSE, alpha = 0.4) +
+        annotate("text", x = rest_x, y = restlabels$y, label = restlabels$restriction, hjust = 0)
+    }
+  }
+  plot
+}
+
