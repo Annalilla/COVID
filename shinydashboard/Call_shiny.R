3b813477e560aba3a32b06426461941dea38fa6f
Annalilla <harman.anna@gmail.com>
2021-04-02 08:28:39 +0200

    Update Call_shiny.R

--- shinydashboard/Call_shiny.R
+++ shinydashboard/Call_shiny.R
@@ -1,7 +1,7 @@
 # Prepares the data for the dashboard and starts the application.
 
-
 library(shinydashboard)
+library(shiny)
 
 source("helpers/Get_and_prepare_data.R")
 source("shinydashboard/Shiny_data_prep.R")
