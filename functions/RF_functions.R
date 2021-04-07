ed5d3092d0badfa5cc9a0024f4ce449a49c45157
Annalilla <harman.anna@gmail.com>
2021-04-07 11:41:50 +0200

    Update RF_functions.R
    rf model

--- functions/RF_functions.R
+++ functions/RF_functions.R
@@ -46,3 +46,40 @@ preproc_predict <- function(rf_country_dat){
   act_rf_dat <- predict(preProcValues, newdata = rf_country_dat)
   return(act_rf_dat)
 }
+                                             
+## Random Forest
+
+rf_model <- function(country_dat){
+  country <- country_dat$country[1]
+  rf_dat_t <- country_dat[complete.cases(country_dat),]
+  
+  # Train and test set
+  set.seed(9985)
+  to_train <- createDataPartition(rf_dat_t$cases_new_cum,
+                                  p = .8,
+                                  list = FALSE,
+                                  times = 1)
+  
+  rf_train <- rf_dat_t[to_train,]
+  rf_test <- rf_dat_t[-to_train,]
+  
+  # RF
+  ctrl <- trainControl(method = "timeslice",
+                       initialWindow = 28,
+                       horizon = 5,
+                       fixedWindow = TRUE)
+  
+  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
+                               round(log(ncol(rf_train)))))
+  
+  rf <- train(as.numeric(cases_new_cum) ~ .,
+              data = rf_train[,-which(colnames(rf_train) %in% c( "cases_new", "date", "last_day", "last_week"))],
+              method = "rf",
+              trControl = ctrl,
+              tuneGrid = grid)
+  
+  
+  rf1 <- rf$finalModel
+  
+  return(rf1)
+}
