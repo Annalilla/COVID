6545e4c72ca2eed4c36ec0f990d61badf697b4ff
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-04-07 04:48:20 +0200

    Final model with timeslice

--- random_forest/Random_forest.R
+++ random_forest/Random_forest.R
@@ -6,288 +6,209 @@ library(randomForest)
 library(Hmisc)
 library(readr)
 library(dplyr)
+library(tidyverse)
 library(stringr)
 library(sjlabelled)
 
 
+source("functions/Get_data.R")
 
-setwd("C:/Users/balan/Documents/IPSDS/MDM/Master_Thesis/Model")
-tdata<- read_rds("C:/Users/balan/Documents/IPSDS/MDM/Master_Thesis/Data/tdata_13_02.rds")
+tdata <- get_data("tdata", local = FALSE)
 
-#source("functions/Data_process_functions.R")
+
+tdata<- read_rds("C:/Users/balan/Documents/IPSDS/MDM/Master_Thesis/Data/tdata.rds")
+
+
+
+# Change variable types (to numerical, factor or date)
+
+# tdata
+vars_to_numerical <- c("testing_rate", "testing_positivity_rate", "tavg", "total_vaccinations", "people_vaccinated",
+                       "people_fully_vaccinated", "new_vaccinations", "new_vaccinations_smoothed", "total_vaccinations_per_hundred",
+                       "people_vaccinated_per_hundred", "people_fully_vaccinated_per_hundred", "new_vaccinations_smoothed_per_million",
+                       "AdaptationOfWorkplace", "AdaptationOfWorkplacePartial", "BanOnAllEvents", "BanOnAllEventsPartial",
+                       "ClosDaycare", "ClosDaycarePartial", "ClosHigh", "ClosHighPartial", "ClosPrim", "ClosPrimPartial",
+                       "ClosPubAny", "ClosPubAnyPartial", "ClosSec", "ClosSecPartial", "ClosureOfPublicTransport",
+                       "ClosureOfPublicTransportPartial", "EntertainmentVenues", "EntertainmentVenuesPartial",
+                       "GymsSportsCentres", "GymsSportsCentresPartial", "HotelsAccommodation", "HotelsAccommodationPartial",
+                       "IndoorOver100", "IndoorOver1000", "IndoorOver1000Partial", "IndoorOver100Partial", "IndoorOver50",
+                       "IndoorOver500", "IndoorOver500Partial", "IndoorOver50Partial", "MasksMandatoryAllSpaces",
+                       "MasksMandatoryAllSpacesPartial", "MasksMandatoryClosedSpaces", "MasksMandatoryClosedSpacesPartial",
+                       "MasksVoluntaryAllSpaces", "MasksVoluntaryAllSpacesPartial", "MasksVoluntaryClosedSpaces",
+                       "MasksVoluntaryClosedSpacesPartial", "MassGatherAll", "MassGatherAllPartial", "NonEssentialShops",
+                       "NonEssentialShopsPartial", "OutdoorOver100", "OutdoorOver1000", "OutdoorOver1000Partial",
+                       "OutdoorOver100Partial", "OutdoorOver50", "OutdoorOver500", "OutdoorOver500Partial", "OutdoorOver50Partial",
+                       "PlaceOfWorship", "PlaceOfWorshipPartial", "PrivateGatheringRestrictions",
+                       "PrivateGatheringRestrictionsPartial", "RegionalStayHomeOrder", "RegionalStayHomeOrderPartial",
+                       "RestaurantsCafes", "RestaurantsCafesPartial", "SocialCircle", "SocialCirclePartial", "StayHomeGen",
+                       "StayHomeGenPartial", "StayHomeOrder", "StayHomeOrderPartial", "StayHomeRiskG", "StayHomeRiskGPartial",
+                       "Teleworking", "TeleworkingPartial", "WorkplaceClosures", "WorkplaceClosuresPartial",
+                       "cases_new", "deaths_new", "recovered_new",
+                       "fb_data.percent_cli", "fb_data.cli_se", "fb_data.percent_cli_unw", "fb_data.cli_se_unw", "fb_data.sample_size_cli",
+                       "fb_data.smoothed_cli", "fb_data.smoothed_cli_se", "fb_data.sample_size_smoothed_cli", "fb_data.percent_mc",
+                       "fb_data.mc_se", "fb_data.percent_mc_unw", "fb_data.mc_se_unw", "fb_data.sample_size_mc", "fb_data.smoothed_mc", 
+                       "fb_data.smoothed_mc_se", "fb_data.sample_size_mc_smoothed", "fb_data.percent_dc", "fb_data.mc_se_dc",
+                       "fb_data.percent_dc_unw", "fb_data.dc_se_unw", "fb_data.sample_size_dc", "fb_data.smoothed_dc", "fb_data.smoothed_dc_se",
+                       "fb_data.sample_size_dc_smoothed")
+
+
+vars_to_factor <- c("country", "country_code", "iso_code", "fb_data.iso_code", "fb_data.country")
+
+vars_to_date <- c("date")
+
+# Convert variables to numerical
+# Handling decimals
+tdata[, which(colnames(tdata) %in% vars_to_numerical)] <-
+  lapply(tdata[, which(colnames(tdata) %in% vars_to_numerical)], function(x) gsub(",", "\\.", x))
+tdata[, which(colnames(tdata) %in% vars_to_numerical)] <-
+  lapply(tdata[, which(colnames(tdata) %in% vars_to_numerical)], as.numeric)
+
+# Convert variables to factor
+tdata[, which(colnames(tdata) %in% vars_to_factor)] <-
+  lapply(tdata[, which(colnames(tdata) %in% vars_to_factor)], as.factor)
+
+# Convert variables to date
+tdata$date <- as.Date(tdata$date)
+
+
+
+source("functions/Data_process_functions.R")
+
 
 ###preprocessing
 
+##Selecting the variables, defining the period with meaningful amount of data at the end of the period
 
-##Selecting the variables
-
-# Some countries are missing from fb data
-mis_c <- c("Cyprus", "Estonia", "Latvia", "Lithuania", "Luxembourg", "Malta")
-rf_dat <- tdata[(((tdata$date >= "2020-05-01") & (tdata$date <= "2021-02-28")) & tdata$country %nin% mis_c), 
-                -which(colnames(tdata) %in% c("testing_new_cases", "tests_done", "testing_population", 
-                                              "testing_rate", "testing_positivity_rate", "fb_data.iso_code","fb_data.country", "fb_status"))]
-
+rf_dat <- tdata[(((tdata$date >= "2020-02-28") & (tdata$date <= "2021-03-28"))), 
+                        -which(colnames(tdata) %in% c("year", "week", "country_code","testing_new_cases", "tests_done", "testing_population", 
+                                              "testing_rate", "testing_positivity_rate", "fb_data.iso_code","fb_data.country", "fb_status",
+                                               "fb_data.cli_se", "fb_data.percent_cli_unw","fb_data.cli_se_unw", "fb_data.sample_size_cli", 
+                                              "fb_data.smoothed_cli", "fb_data.smoothed_cli_se", "fb_data.sample_size_smoothed_cli",
+                                              "fb_data.mc_se", "fb_data.percent_mc_unw", "fb_data.mc_se_unw", "fb_data.sample_size_mc",
+                                              "fb_data.smoothed_mc", "fb_data.smoothed_mc_se", "fb_data.sample_size_mc_smoothed",
+                                              "fb_data.mc_se_dc", "fb_data.percent_dc_unw", "fb_data.dc_se_unw", "fb_data.sample_size_dc",               
+                                              "fb_data.smoothed_dc", "fb_data.smoothed_dc_se", "fb_data.sample_size_dc_smoothed",      
+                                              "new_vaccinations_smoothed", "iso_code", "people_vaccinated", "total_vaccinations", 
+                                              "new_vaccinations_smoothed", "people_fully_vaccinated", "new_vaccinations_smoothed_per_million"))]
+                
 
 
 ##Outcome variable
 
 #cumulative cases_new
+rf_dat$cases_new_cum <- cumsum(rf_dat$cases_new)
 
-rf_dat$cum_cases_new <- cumsum(rf_dat$cases_new)
+# Smooth cases_new and cumulative cases_new with rolling average window = 7 days
+rf_dat$cases_new_cum <- rollmean(rf_dat$cases_new_cum, 7, fill = NA)
+rf_dat$cases_new<- rollmean(rf_dat$cases_new, 7, fill = NA)
 
-label(rf_dat$cum_cases_new) <- "Cumulative confirmed daily new cases" 
-
-length(which(rf_dat$cases_new <0)) #12
-
-# Smooth cumulative cases_new with rolling average window = 7 days
-rf_dat$cumra_cases_new <- rollmean(rf_dat$cum_cases_new, 7, fill = NA)
-
-label(rf_dat$cumra_cases_new) <- "7-day rolling average of cumulative confirmed daily new cases" 
-
-str(rf_dat$cumra_cases_new)
-summary(rf_dat$cum_cases_new)
 
 ##Predictors
 
-#Rescaling to [0,1]
+#vaccination data: change NAs to 0 after vaccination started
 
-#remove labels (preProcValues cannot handle labels, ruins factor and date variables)
-pprf_dat <- remove_all_labels(rf_dat)
-str(pprf_dat)
+vacc <-c("new_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated_per_hundred", "people_fully_vaccinated_per_hundred")
 
-pprf_dat$country <- as.factor(pprf_dat$country)
-pprf_dat$iso_code <-as.factor(pprf_dat$iso_code)
-pprf_dat$date <-as.Date(pprf_dat$date)
+rf_dat <- replace_na_after_first_vacc(rf_dat) 
 
 
-preProcValues <- preProcess(pprf_dat[, -which(colnames(pprf_dat) %in% c("cases_new", "cum_cases_new", "cumra_cases_new", "date", "year", "week",
-                                                                        "WorkplaceClosuresPartial", "country_code"))], method = "range")
-preProcValues
-
-transformed <- predict(preProcValues, newdata = pprf_dat )
-head(transformed)
-
-
-#find and remove highly correlated predictors
-
-#make a correlation matrix of the numerical predictors
-
-cm <- cor(as.matrix(transformed[, -which(colnames(transformed) %in% c("cases_new", "cum_cases_new", "cumra_cases_new", "date", "year", "week",
-                                                                "WorkplaceClosuresPartial", "country_code", "iso_code", "country"))]))
-
-str(cm)
-summary(cm[upper.tri(cm)])
-#Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
-#-0.6898 -0.0786 -0.0194  0.0174  0.1006  0.6698    2646 
-
-#No high correlation, no need to remove any predictors
-#highlyCorPred <-findCorrelation(cm, cutoff = 0.9)
-#filtered_trans <- filtered_trans[,-highlyCorPred]
-
-# Test *******
-# First only with a few variables and for Austria
-rf_dat <- rf_dat[which(rf_dat$country == "Austria"), c("date", "cases_new", "ClosDaycare", "ClosPubAny", "StayHomeOrder",
-                                                       "fb_data.percent_mc", "fb_data.percent_dc", "tavg")]
-rf_dat <- rf_dat[complete.cases(rf_dat),]
-
-# visualize
-rf_dat %>% 
-  ggplot(aes(date, cases_new)) +
-  geom_line() +
-  theme_minimal() +
-  labs(title = "Number of new COVID infections in Austria", x = "date", y = "number of cases")
-
-# Train and test set
-set.seed(9985)
-to_train <- createDataPartition(rf_dat$cases_new,
-                                p = .8,
-                                list = FALSE,
-                                times = 1)
-
-rf_train <- rf_dat[to_train,]
-rf_test <- rf_dat[-to_train,]
-
-# RF
-ctrl <- trainControl(method = "cv",
-                     number = 10,
-                     verboseIter = TRUE)
-
-grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
-                             round(log(ncol(rf_train)))))
-
-rf <- train(as.numeric(cases_new) ~ .,
-            data = rf_train[,-which(colnames(rf_train) == "date")],
-            method = "rf",
-            trControl = ctrl,
-            tuneGrid = grid)
-
-rf1 <- rf$finalModel
-
-# Interpretation
-varImpPlot(rf1)
-importance(rf1)
-varImp(rf1)
-
-rf1
-
-# rf1
-
-#Call:
-#  randomForest(x = x, y = y, mtry = param$mtry) 
-#Type of random forest: regression
-#Number of trees: 500
-#No. of variables tried at each split: 2
-
-#Mean of squared residuals: 1747538
-#% Var explained: 52.47
-
-
-
-# RF
-ctrl <- trainControl(method = "cv",
-                     number = 10,
-                     verboseIter = TRUE)
-
-grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
-                             round(log(ncol(rf_train)))))
-
-
-
-rf <- train(as.numeric(cum_cases_new) ~ .,
-            data = rf_train[,-which(colnames(rf_train) %in% c("date", "cases_new"))],
-            method = "rf",
-            trControl = ctrl,
-            tuneGrid = grid)
-
-rf1.1 <- rf$finalModel
-
-# Interpretation
-varImpPlot(rf1.1)
-importance(rf1.1)
-varImp(rf1.1)
-
-rf1.1
-
-#> rf1.1
-
-#Call:
-#  randomForest(x = x, y = y, mtry = param$mtry) 
-#Type of random forest: regression
-#Number of trees: 500
-#No. of variables tried at each split: 3
-
-#Mean of squared residuals: 2272740939
-#% Var explained: 89.19
-
-# RF with timeslice
-
-
-
-myTimeControl <- trainControl(method = "timeslice", initialWindow = 36, horizon = 12, fixedWindow = TRUE)
-
-plsFitTime <- train(unemploy ~ pce + pop + psavert, data = economics, method = "pls",
-                    preProc = c("center", "scale"), trControl = myTimeControl)
-
-A better example can be found here: https://r-norberg.blogspot.com/2016/08/data-splitting-time-slices-with.html. It include a function for irregular timeslices!
-ctrl <- trainControl(method = "timeslice",
-                     initialWindow = 28,
-                     horizon = 1,
-                     fixedWindow = TRUE)
-
-rf <- train(as.numeric(cases_new) ~ .,
-            data = rf_train[,-which(colnames(rf_train) == "date")],
-            method = "rf",
-            trControl = ctrl)
-
-rf2 <- rf$finalModel
-
-# Interpretation
-varImpPlot(rf2)
-importance(rf2)
-varImp(rf2)
-
-rf2
-
-###########################
-# With modified data
-
-rf_dat <- tdata[(((tdata$date >= "2020-05-01") & (tdata$date <= "2021-02-28")) & tdata$country %nin% mis_c), -which(colnames(tdata) %in% c("testing_new_cases", "tests_done", "testing_population",
-                                                                                                                                           "testing_rate", "testing_positivity_rate", "fb_data.iso_code",
-                                                                                                                                           "fb_data.country", "fb_status"))]
-rf_dat <- rf_dat[which(rf_dat$country == "Austria"), c("date", "cases_new", "ClosDaycare", "ClosPubAny", "StayHomeOrder",
-                                                       "ClosDaycarePartial", "ClosPubAnyPartial", "StayHomeOrderPartial",
-                                                       "fb_data.percent_mc", "fb_data.percent_dc", "tavg")]
 
 # Lead for new cases
-rf_dat$cases_new <- lead(rf_dat$cases_new, 14)
-
-# Smooth cases with rolling average window = 7 days
-rf_dat$cases_new <- rollmean(rf_dat$cases_new, 7, fill = NA)
+rf_dat$cases_new_lead <- lead(rf_dat$cases_new, 14)
 
 # Variable for number of cases on previous day and week
 rf_dat$last_day <- lag(rf_dat$cases_new, 1)
 rf_dat$last_week <- lag(rf_dat$cases_new, 7)
 
-# Smooth temperature and fb variables with rolling average window = 7 days
-rf_dat$tavg <- rollmean(rf_dat$tavg, 7, fill = NA)
-rf_dat$fb_data.percent_mc <- rollmean(rf_dat$fb_data.percent_mc, 7, fill = NA)
-rf_dat$fb_data.percent_dc <- rollmean(rf_dat$fb_data.percent_dc, 7, fill = NA)
+# Smooth deaths, recovered, temperature, fb and vaccination variables with rolling average window = 7 days
 
-# Missing values
-rf_dat <- rf_dat[complete.cases(rf_dat),]
+vars_to_smooth <- c("deaths_new", "recovered_new", "tavg", "fb_data.percent_cli", "fb_data.percent_mc", "fb_data.percent_dc", 
+                    "new_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated_per_hundred", 
+                    "people_fully_vaccinated_per_hundred")
+
+
+rf_dat[, which(colnames(rf_dat) %in% vars_to_smooth)] <-
+  lapply(rf_dat[, which(colnames(rf_dat) %in% vars_to_smooth)], rollmean, 7, fill = NA)
+
+
+#Standardize by countries
+
+#IF data labelled: remove labels (preProcValues cannot handle labels, ruins factor and date variables)
+#rf_dat <- remove_all_labels(rf_dat)
+
+#rf_dat$country <- as.factor(rf_dat$country)
+#rf_dat$date <-as.Date(rf_dat$date)
+
+#Split by countries
+c_rf_dat <- split(rf_dat, rf_dat$country)
+
+#Standardize
+rf_dat <- lapply(c_rf_dat, function(x) preproc_predict(x))
+rf_dat <- do.call(rbind, rf_dat)
+
+#find and remove highly correlated predictors
+
+#make a correlation matrix of the numerical predictors
+
+cm <- cor(as.matrix(rf_dat[, -which(colnames(rf_dat) %in% c("cases_new", "cases_new_cum", "date", "year", "week",
+                                                                 "country_code", "iso_code", "country"))]))
+summary(cm[upper.tri(cm)])
+
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+#-0.6416 -0.0562  0.0000  0.0217  0.0769  0.8517    3135 
+
+#highlyCorPred <-findCorrelation(cm, cutoff = 0.70)
+
+#No high correlation, no need to remove any predictors
 
 # Merging partially and not partially applied restrictions
-rf_dat <- merge_all_partial_rest(rf_dat)
 
-######
+dat <- rf_dat
+rf_dat_p <- merge_all_partial_rest(dat)
+
+
+
+#### RF with timeslice, cumulative smoothed outcome, partial restrictions not merged
+
+#data should be sorted by date!
+
+
+# Only for Hungary
+rf_dat_t <- rf_dat[which(rf_dat$country == "Hungary"), ]
+rf_dat_t <- rf_dat_t[complete.cases(rf_dat_t),]
+
 # Train and test set
 set.seed(9985)
-to_train <- createDataPartition(rf_dat$cases_new,
+to_train <- createDataPartition(rf_dat_t$cases_new_cum,
                                 p = .8,
                                 list = FALSE,
                                 times = 1)
 
-rf_train <- rf_dat[to_train,]
-rf_test <- rf_dat[-to_train,]
+rf_train <- rf_dat_t[to_train,]
+rf_test <- rf_dat_t[-to_train,]
 
-#RF
-ctrl <- trainControl(method = "cv",
-                     number = 10,
-                     verboseIter = TRUE)
+# RF
+ctrl <- trainControl(method = "timeslice",
+                     initialWindow = 28,
+                     horizon = 5,
+                     fixedWindow = TRUE)
 
 grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
                              round(log(ncol(rf_train)))))
 
-rf <- train(as.numeric(cases_new) ~ .,
-            data = rf_train[,-which(colnames(rf_train) == "date")],
+rf <- train(as.numeric(cases_new_cum) ~ .,
+            data = rf_train[,-which(colnames(rf_train) %in% c( "cases_new", "date", "last_day", "last_week"))],
             method = "rf",
             trControl = ctrl,
             tuneGrid = grid)
 
-rf3 <- rf$finalModel
+
+rf1 <- rf$finalModel
 
 # Interpretation
-varImpPlot(rf3)
-importance(rf3)
-varImp(rf3)
-rf3
+varImpPlot(rf1)
+varImp(rf1)
 
-# RF with timeslice and modified data
-ctrl <- trainControl(method = "timeslice",
-                     initialWindow = 28,
-                     horizon = 1,
-                     fixedWindow = TRUE)
+rf1
 
-rf <- train(as.numeric(cases_new) ~ .,
-            data = rf_train[,-which(colnames(rf_train) == "date")],
-            method = "rf",
-            trControl = ctrl)
 
-rf4 <- rf$finalModel
-
-# Interpretation
-varImpPlot(rf4)
-importance(rf4)
-varImp(rf4)
-
-rf4
