b4a9503a88d1e01a5c4b21b1bdae8a80e309bda3
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-04-11 12:13:51 +0200

    Creats pdp inputs and plots them in one function

--- functions/pdp_functions.R
+++ functions/pdp_functions.R
@@ -1,8 +1,39 @@
-#Creates pdp plots for all countries (with FB data), for one predictor
+## Creates the train object per countries, then plots pdps for countries
 
-plot_pdp <-function(country_train) {
-  train <- country_train$country[1]
-  pdp_plots <- pdp::partial(rf, pred.var = "tavg", plot=TRUE)
+pdp_plots <- function(country_dat){
+  country <- country_dat$country[1]
+  rf_dat_t <- country_dat[complete.cases(country_dat),]
+  
+  # Train and test set
+  set.seed(9985)
+  to_train <- createDataPartition(rf_dat_t$cases_new_cum,
+                                  p = .8,
+                                  list = FALSE,
+                                  times = 1)
+  
+  rf_train <- rf_dat_t[to_train,]
+  rf_test <- rf_dat_t[-to_train,]
+  
+  # RF
+  ctrl <- trainControl(method = "timeslice",
+                       initialWindow = 28,
+                       horizon = 5,
+                       fixedWindow = TRUE)
+  
+  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
+                               round(log(ncol(rf_train)))))
+  
+  rf <- train(as.numeric(cases_new_cum) ~ .,
+              data = rf_train[,-which(colnames(rf_train) %in% c( "cases_new", "date", "last_day", "last_week"))],
+              method = "rf",
+              trControl = ctrl,
+              tuneGrid = grid)
+  
+  pdp_plots <- pdp::partial(rf, pred.var = "tavg", plot=TRUE, rug=TRUE)
   
   return(pdp_plots)
 }
+
+
+
+
