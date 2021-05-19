237912135797d5f77e9d939170d508085e0f8b5f
Annalilla <harman.anna@gmail.com>
2021-05-19 10:23:59 +0200

    Update Create_database.R

--- data_collection/Create_database.R
+++ data_collection/Create_database.R
@@ -4,6 +4,9 @@ source("data_collection/Save_data.R")
 source("data_collection/Collect_data.R")
 source("data_collection/Merge_data.R")
 
+# Country_char: numerical to character
+country_char <- as.data.frame(apply(country_char, 2, as.character))
+
 # Save local
 save_data(country_char, datname = "country_char", dirname = "data", archieve = TRUE)
 save_data(tdata, datname = "tdata", dirname = "data", archieve = TRUE)
