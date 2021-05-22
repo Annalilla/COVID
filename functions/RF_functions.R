df93646b21e3708552a4cf099cc05c94a8a49088
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-05-22 23:53:55 +0200

    Update on timeslice (no prior train/test set split)

--- functions/RF_functions.R
+++ functions/RF_functions.R
@@ -55,13 +55,14 @@ rf_model <- function(country_dat){
   
   # Train and test set
   set.seed(9985)
-  to_train <- createDataPartition(rf_dat_t$cases_new_cum,
-                                  p = .8,
-                                  list = FALSE,
-                                  times = 1)
+  #For timeslices no need to split data into train/test set before the 'train' function.
+  #to_train <- createDataPartition(rf_dat_t$cases_new_cum,
+  #                               p = .8,
+  #                                list = FALSE,
+  #                                times = 1)
   
-  rf_train <- rf_dat_t[to_train,]
-  rf_test <- rf_dat_t[-to_train,]
+  #rf_train <- rf_dat_t[to_train,]
+  #rf_test <- rf_dat_t[-to_train,]
   
   # RF
   ctrl <- trainControl(method = "timeslice",
@@ -69,11 +70,11 @@ rf_model <- function(country_dat){
                        horizon = 5,
                        fixedWindow = TRUE)
   
-  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
-                               round(log(ncol(rf_train)))))
+  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_dat_t))),
+                               round(log(ncol(rf_dat_t)))))
   
   rf <- train(as.numeric(cases_new_cum) ~ .,
-              data = rf_train[,-which(colnames(rf_train) %in% c("country", "cases_new", "date", "last_day", "last_week"))],
+              data = rf_dat_t[,-which(colnames(rf_dat_t) %in% c("country", "cases_new", "date", "last_day", "last_week"))],
               method = "rf",
               trControl = ctrl,
               tuneGrid = grid)
