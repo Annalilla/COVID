cb92b3ff07471b73df45d121bafa2515eb407de2
Annalilla <harman.anna@gmail.com>
2021-05-28 10:11:25 +0200

    Update Hierarchical_cluster.R

--- cluster/Hierarchical_cluster.R
+++ cluster/Hierarchical_cluster.R
@@ -4,6 +4,7 @@ library(stats)
 library(factoextra)
 library(tidyverse)
 library(cowplot)
+library(maps)
 
 # Remove countries with no fb data
 c_remove <- c("CY", "EE", "LV", "LT", "LU", "MT")
@@ -164,3 +165,29 @@ age_groups <- ggplot(agegrl, aes(x = variable, y = value, group = cluster)) +
   theme_minimal()
 ggsave("age_groups.png", age_groups, width = 10, height = 6)
 
+# Visualization of clusters on a map
+# Prepare data with clusters
+map_cluster <- cbind(clust_dat, "country_code" = rownames(clust_dat))
+map_cluster <- merge(map_cluster, capitals[, c("country", "country_code_iso2")], by.x = "country_code", by.y = "country_code_iso2")
+map_cluster <- map_cluster %>%
+  select(country, groups) %>%
+  rename(region = country, cluster = groups)
+map_cluster$cluster <- as.factor(map_cluster$cluster)
+# Replace "Czechia" with "Czech Republic" to match with the maps
+map_cluster[which(map_cluster$region == "Czechia"), "region"] <- "Czech Republic"
+
+# Selecting countries to visualize
+map_countries <- c(unique(capitals$country), "Czech Republic")
+map <- map_data("world")
+eu_map <- map_data("world", region = map_countries)
+eu_clusters_map <- left_join(map_cluster, eu_map, by = "region")
+
+region_lab <- eu_clusters_map %>%
+  group_by(region) %>%
+  summarise(long = mean(long), lat = mean(lat))
+
+ggplot(eu_clusters_map, aes(long, lat)) +
+  geom_polygon(aes(group = group, fill = cluster), color = "white") +
+  geom_text(aes(label = region), data = region_lab,  size = 3, hjust = 0.5) +
+  scale_fill_manual(values=c("#F8766D", "#B79F00", "#00BA38", "#38CBCE", "#68A0FE", "#F564E3", "#fb61d7")) +
+  theme_void()
