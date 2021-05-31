18c9bbf564ff29fc2dc3f4b54433dfd8e6d33178
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-06-01 00:56:49 +0200

    Update Shiny_data_prep.R

--- shinydashboard/Shiny_data_prep.R
+++ shinydashboard/Shiny_data_prep.R
@@ -88,14 +88,14 @@ rf_train <- function(country_dat){
   rf_dat_t <- country_dat[complete.cases(country_dat),]
   
   # Train and test set
-  set.seed(9985)
-  to_train <- createDataPartition(rf_dat_t$cases_new_cum,
-                                  p = .8,
-                                  list = FALSE,
-                                  times = 1)
+  #set.seed(9985)
+  #to_train <- createDataPartition(rf_dat_t$cases_new_cum,
+  #                                p = .8,
+  #                                list = FALSE,
+  #                                times = 1)
   
-  rf_train <- rf_dat_t[to_train,]
-  rf_test <- rf_dat_t[-to_train,]
+  #rf_train <- rf_dat_t[to_train,]
+  #rf_test <- rf_dat_t[-to_train,]
   
   # RF
   ctrl <- trainControl(method = "timeslice",
@@ -103,11 +103,11 @@ rf_train <- function(country_dat){
                        horizon = 5,
                        fixedWindow = TRUE)
   
-  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_train))),
-                               round(log(ncol(rf_train)))))
+  grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_dat_t))),
+                               round(log(ncol(rf_dat_t)))))
   
   rf <- train(as.numeric(cases_new_cum) ~ .,
-              data = rf_train[,-which(colnames(rf_train) %in% c( "cases_new", "date", "last_day", "last_week", "country"))],
+              data = rf_dat_t[,-which(colnames(rf_dat_t) %in% c( "cases_new", "date", "last_day", "last_week", "country"))],
               method = "rf",
               trControl = ctrl,
               tuneGrid = grid)
