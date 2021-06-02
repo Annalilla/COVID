3ef5d2947e6ecb6df04af310f672472fff999009
Annalilla <harman.anna@gmail.com>
2021-06-02 11:19:58 +0200

    Update Get_and_prepare_data.R

--- helpers/Get_and_prepare_data.R
+++ helpers/Get_and_prepare_data.R
@@ -7,4 +7,4 @@ tdata <- get_data("tdata", local = FALSE)
 country_char <- get_data("country_char", local = FALSE)
 
 source("helpers/Change_variable_types.R")
-source("helpers/Add_variable_labels.R")
+#source("helpers/Add_variable_labels.R")
