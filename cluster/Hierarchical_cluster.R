15c3bf6d8371944da00e7a1b7dd0d265ef0a6a83
Annalilla <harman.anna@gmail.com>
2021-06-02 17:52:32 +0200

    Update Hierarchical_cluster.R

--- cluster/Hierarchical_cluster.R
+++ cluster/Hierarchical_cluster.R
@@ -170,8 +170,8 @@ ggsave("age_groups.png", age_groups, width = 10, height = 6)
 map_cluster <- cbind(clust_dat, "country_code" = rownames(clust_dat))
 map_cluster <- merge(map_cluster, capitals[, c("country", "country_code_iso2")], by.x = "country_code", by.y = "country_code_iso2")
 map_cluster <- map_cluster %>%
-  select(country, groups) %>%
-  rename(region = country, cluster = groups)
+  dplyr::select(country, groups) %>%
+  dplyr::rename(region = country, cluster = groups)
 map_cluster$cluster <- as.factor(map_cluster$cluster)
 # Replace "Czechia" with "Czech Republic" to match with the maps
 map_cluster[which(map_cluster$region == "Czechia"), "region"] <- "Czech Republic"
