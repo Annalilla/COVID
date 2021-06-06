6992a828f62b8c31d86db980ebde1649caeacc09
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-06-07 00:04:58 +0200

    Add files via upload

--- shinydashboard/Call_shiny.R
+++ shinydashboard/Call_shiny.R
@@ -3,8 +3,12 @@
 library(shinydashboard)
 library(shiny)
 
+
 source("helpers/Get_and_prepare_data.R")
 source("random_forest/Random_forest.R")
+source("cluster/Hierarchical_cluster.R")
+source("random_forest/Cluster_RF.R")
+source("random_forest/rank_corr.R")
 source("shinydashboard/Shiny_data_prep.R")
 
 runApp("shinydashboard")
