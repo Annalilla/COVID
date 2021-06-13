ad9f823c35a7855e21aacf6b05fc791ee1506731
Annalilla <harman.anna@gmail.com>
2021-06-13 11:46:12 +0200

    Update Create_database.R

--- data_collection/Create_database.R
+++ data_collection/Create_database.R
@@ -4,8 +4,11 @@ source("data_collection/Save_data.R")
 source("data_collection/Collect_data.R")
 source("data_collection/Merge_data.R")
 
-# Country_char: numerical to character **RUN ONLY if you would like to save the data online. If you would like to work with the country_char database right now, DON'T RUN
-country_char <- as.data.frame(apply(country_char, 2, as.character))
+# Country_char: numerical to character **RUN THIS ROW ONLY if you would like to save the data online. If you would like to work with the country_char database right now, DON'T RUN
+country_char <- as.data.frame(apply(country_char, 2, as.character
+                                    
+# tdata only until maximal date set in prepare_run.R
+tdata <- tdata[tdata$date <= maxdate,]
 
 # Save local
 save_data(country_char, datname = "country_char", dirname = "data", archieve = TRUE)
