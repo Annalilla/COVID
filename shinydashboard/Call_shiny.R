884feaab43dc37a8c8018901a623fbe1101f415c
Annalilla <harman.anna@gmail.com>
2021-06-21 20:37:56 +0200

    Update Call_shiny.R

--- shinydashboard/Call_shiny.R
+++ shinydashboard/Call_shiny.R
@@ -5,8 +5,8 @@ library(shiny)
 
 
 source("helpers/Get_and_prepare_data.R")
-source("random_forest/Random_forest.R")
 source("cluster/Hierarchical_cluster.R")
+source("random_forest/Random_forest.R")
 source("random_forest/Cluster_RF.R")
 source("random_forest/rank_corr.R")
 source("shinydashboard/Shiny_data_prep.R")
