a024d573660aa28be7a848235029083ba97969e7
Annalilla <harman.anna@gmail.com>
2021-10-06 10:59:21 +0200

    Update Cluster_RF.R

--- random_forest/Cluster_RF.R
+++ random_forest/Cluster_RF.R
@@ -1,4 +1,6 @@
-# Random Forest models on countries
+## Create a time-varying dbase including clusters, then computes repeated variable importance for each cluster. 
+#The result is the input for the rank correlation of the 'Country characteristics' tab.
+
 
 library(caret)
 library(ranger)
@@ -11,24 +13,23 @@ library(tidyverse)
 library(stringr)
 library(sjlabelled)
 
-source("functions/RF_functions.R")
+
+##Run RF on clusters
+
+source("functions/RF_cluster_functions.R")
 
 
 ###preprocessing
 
 ##Selecting the variables, defining the period with meaningful amount of data at the end of the period
-
-tdata <- as.data.frame(tdata)
-
-# Maximum date: fb direct contact and mask coverage available in all countries
-rf_max_date <- min(do.call(c, lapply(unique(tdata$country), function(x){
+rf_max_date <- min(as.Date(do.call(c, lapply(unique(tdata$country), function(x){
   act_country <- tdata[tdata$country == x,]
   if(!all(is.na(act_country$fb_data.percent_mc)) & !all(is.na(act_country$fb_data.percent_dc))){
     max(act_country[!is.na(act_country$fb_data.percent_mc) & !is.na(act_country$fb_data.percent_dc), "date"])
   }
-})))
+}))))
 
-rf_dat <- tdata_cl[(((tdata_cl$date >= "2020-02-28") & (tdata_cl$date <= rf_max_date))), 
+rf_dat_cl <- tdata_cl[(((tdata_cl$date >= "2020-02-28") & (tdata_cl$date <= rf_max_date))), 
                       -which(colnames(tdata_cl) %in% c("year", "week", "country_code","testing_new_cases", "tests_done", "testing_population", 
                                                        "testing_rate", "testing_positivity_rate", "fb_data.iso_code","fb_data.country", "fb_status",
                                                        "fb_data.cli_se", "fb_data.percent_cli_unw","fb_data.cli_se_unw", "fb_data.sample_size_cli", 
@@ -41,29 +42,24 @@ rf_dat <- tdata_cl[(((tdata_cl$date >= "2020-02-28") & (tdata_cl$date <= rf_max_
                                                        "deaths_new", "recovered_new"))]
 
 
+
 ##Outcome variable
 
 # Number of cases proportionate to population size
-rf_dat$cases_new <- 100 * rf_dat$cases_new/rf_dat$`Population size`
+rf_dat_cl$cases_new <- 100 * rf_dat_cl$cases_new/rf_dat_cl$`Population size`
 
 # Lead to new cases
-rf_dat$cases_new <- lead(rf_dat$cases_new, 14)
+rf_dat_cl$cases_new <- lead(rf_dat_cl$cases_new, 14)
 
-# Removing group and country characteristics
-rf_dat <- rf_dat[, -which(colnames(rf_dat) %in% c("groups", "Population size", "Males", "Health Expenditures", "Cultural participation", "Under 20",
-                                                           "20-39" , "40-59", "60-79", "Above 80"))]
-
-
-# Preprocess per country
+# Preprocess per cluster
 # Split by country
-rf_dat_splitted <- split(rf_dat, rf_dat$country)
-rf_dat_splitted <- lapply(rf_dat_splitted, function(x){
+rf_dat_cl_splitted <- split(rf_dat_cl, rf_dat_cl$country)
+rf_dat_cl_splitted <- lapply(rf_dat_cl_splitted, function(x){
   # cumulative cases_new
   x$cases_new[which(is.na(x$cases_new))] <- 0
   x$cases_new_cum <- cumsum(x$cases_new)
   
   # smooth cases_new and cumulative cases_new with rolling average window = 7 days
-  
   x$cases_new_cum <- rollmean(x$cases_new_cum, 7, fill = NA)
   x$cases_new<- rollmean(x$cases_new, 7, fill = NA)
   
@@ -72,136 +68,47 @@ rf_dat_splitted <- lapply(rf_dat_splitted, function(x){
   x$last_week <- lag(x$cases_new, 21)
   
   # Smooth deaths, recovered, temperature, fb and vaccination variables with rolling average window = 7 days
-  vars_to_smooth <- c("deaths_new", "recovered_new", "tavg", "fb_data.percent_cli", "fb_data.percent_mc", "fb_data.percent_dc") 
+  vars_to_smooth <- c("deaths_new", "recovered_new", "tavg", "fb_data.percent_cli", "fb_data.percent_mc", "fb_data.percent_dc")
+  
   
   x[, which(colnames(x) %in% vars_to_smooth)] <-
     lapply(x[, which(colnames(x) %in% vars_to_smooth)], rollmean, 7, fill = NA)
   
-  # Standardize
-  x <- preproc_predict(x)
   
   x
 })
-rf_dat <- do.call("rbind", rf_dat_splitted)
+
+rf_dat_cl <- do.call("rbind", rf_dat_cl_splitted)
+
+# Split and preprocess by cluster
+rf_dat_cl_splitted <- split(rf_dat_cl, rf_dat_cl$groups)
+rf_dat_cl_splitted <- lapply(rf_dat_cl_splitted, function(x){
+  
+  # Standardize
+  x <- preproc_predict_cl(x)
+  
+  x$groups <- as.factor(x$groups)
+  
+  x
+})
 
 
-#Standardize by countries
+cl_rf_dat_fb <- rf_dat_cl_splitted
 
-#IF data labelled: remove labels (preProcValues cannot handle labels, ruins factor and date variables)
-#rf_dat <- remove_all_labels(rf_dat)
+cluster_res_varimp <- lapply(cl_rf_dat_fb, function(x) rf_model_cl(x, wind = 28, hori = 5))
 
-#rf_dat$country <- as.factor(rf_dat$country)
-#rf_dat$date <-as.Date(rf_dat$date)
+#Format data for the 'Country-char vs.varimp' tab input
+cluster_res_varimp <- do.call(rbind, cluster_res_varimp)
 
-#Split by countries
-#c_rf_dat <- split(rf_dat, rf_dat$country)
+cluster_res_varimp$groups <- rownames(cluster_res_varimp)
 
-#Standardize
-#rf_dat <- lapply(c_rf_dat, function(x) preproc_predict(x))
-#rf_dat <- do.call(rbind, rf_dat)
+cluster_res_varimp$groups <- substr(cluster_res_varimp$groups, 1, 1)        # Extract first characters (cluster numbers)
 
-#find and remove highly correlated predictors
-
-#make a correlation matrix of the numerical predictors
-
-cm <- cor(as.matrix(rf_dat[, -which(colnames(rf_dat) %in% c("cases_new", "cases_new_cum", "date", "year", "week",
-                                                                 "country_code", "iso_code", "country"))]))
-summary(cm[upper.tri(cm)])
-
-#Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
-#-0.6884 -0.0602  0.0000  0.0185  0.0800  0.9056     399  
-
-#highlyCorPred <-findCorrelation(cm, cutoff = 0.70)
-
-#No high correlation, no need to remove any predictors
-
-# Merging partially and not partially applied restrictions
-
-#dat <- rf_dat
-#rf_dat_p <- merge_all_partial_rest(dat)
-
-
-
-### RF with timeslice, cumulative smoothed outcome, partial restrictions not merged, with REPEATED variable importance
-#for more robust result.
-
-#data should be sorted by date (within country)!
-
-
-# Only for Hungary
-#rf_dat_r <- rf_dat[which(rf_dat$country == "Hungary"), ]
-#rf_dat_r <- rf_dat_r[complete.cases(rf_dat_r),]
-
-
-#set.seed(9985)
-
-# RF
-#ctrl <- trainControl(method = "timeslice",
-#                     initialWindow = 28,
-#                     horizon = 5,
-#                     fixedWindow = TRUE)
-
-#grid <- expand.grid(mtry = c(round(sqrt(ncol(rf_dat_r))),
-#                             round(log(ncol(rf_dat_r)))))
-
-#rf_r <- train(as.numeric(cases_new_cum) ~ .,
-#            data = rf_dat_r[,-which(colnames(rf_dat_r) %in% c("country", "cases_new", "date", "last_day", "last_week"))],
-#            method = "rf",
-#           trControl = ctrl,
-#            tuneGrid = grid)
-
-## create iml predictor
-
-#create features data (without outcome cases_new_cum)
-#feat <- rf_dat_r[,-which(colnames(rf_dat_r) %in% c("cases_new_cum", "country", "cases_new", "date", "last_day", "last_week"))]
-#predictor <- Predictor$new(model = rf_r, data = feat, y = as.numeric(rf_dat_r$cases_new_cum))
-# calculate feature importance
-#permimp <- FeatureImp$new(predictor, loss = "mae", compare = "ratio", n.repetitions = 5)
-
-
-#plot(permimp)
-#permimp$results$
-#permimp$results$importance
-#permimp$results[, c("feature", "importance")]
-
-#rf2 <- rf_r$finalModel
-
-
-# Interpretation
-#varImpPlot(rf2)
-#varImp(rf2)
-
-#rf2
-
-## All countries (with fb data)
-
-# Removing countries without fb data
-# rf_dat_fb <- rf_dat[-which(rf_dat$country %in% c("Cyprus", "Estonia", "Latvia", "Lithuania", "Luxembourg", "Malta")),]
-# Refactor country
-rf_dat_fb <- rf_dat
-rf_dat_fb$country <- as.factor(as.character(rf_dat_fb$country))
-
-# Split by countries
-c_rf_dat_fb <- c_rf_dat <- split(rf_dat_fb, rf_dat_fb$country)
-
-# Create Random Forest input for PDP tab
-country_res <- lapply(c_rf_dat_fb, function(x) rf_model_pdp(x))
 
 #Save results and further input for the Shiny visualization
 
 dir.create("shinydashboard", showWarnings = FALSE)
 dir.create("shinydashboard/dat", showWarnings = FALSE)
 
-#saveRDS(country_res, "shinydashboard/dat/country_res.RDS")
-saveRDS(c_rf_dat_fb, "shinydashboard/dat/c_rf_dat_fb.RDS")
-saveRDS(rf_dat_fb, "shinydashboard/dat/rf_dat_fb.RDS")
-
-#p <- varImpPlot(country_res[[8]])
-#p + title(names(country_res)[8])
-
-
-# Create Random Forest input (repeated varimp) for 'Bump Chart' and 'Country characteristics' tab
-country_res_varimp <- lapply(c_rf_dat_fb, function(x) rf_model_varimp(x))
-
-#Save results and further input for the Shiny visualization
-saveRDS(country_res_varimp, "shinydashboard/dat/country_res_varimp.RDS")
+saveRDS(cluster_res_varimp, "shinydashboard/dat/cluster_res_varimp.RDS")
+saveRDS(cl_rf_dat_fb, "shinydashboard/dat/cl_rf_dat_fb.RDS")
