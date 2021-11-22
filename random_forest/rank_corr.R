a26c23db2965a08def945f3de500b8c0a48d0a02
aniko-b <40137625+aniko-b@users.noreply.github.com>
2021-11-22 01:18:41 +0100

    updated with within-cluster comparisons

--- random_forest/rank_corr.R
+++ random_forest/rank_corr.R
@@ -1,5 +1,12 @@
 # Computes correlation between the repeated varimp rank of the clusters and the repeated varimp rank of the countries
 
+
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
+
+
+
 country_res_varimp <- readRDS("shinydashboard/dat/country_res_varimp.RDS")
 cluster_res_varimp <- readRDS("shinydashboard/dat/cluster_res_varimp.RDS")
 
@@ -93,3 +100,89 @@ rank_corr_res <- lapply(c(1:max(clust_dat$groups)), rc_cluster)
 
 # Save results for the visualization 'Country characteristics tab'
 saveRDS(rank_corr_res, "shinydashboard/dat/rank_corr_res.RDS")
+
+
+
+# Computes correlation between the repeated varimp rank of the clusters
+
+  
+
+# function to calculate correlation between varimp rank of the clusters
+
+rc_btw_cluster <- function(cluster_btw){
+  act_clust_1 <- clust_geo[clust_geo$groups == cluster_btw,]
+  act_clust_2 <- clust_geo[clust_geo$groups > cluster_btw,]
+  
+  
+  act_cor <- unlist(lapply(act_clust_2, function(x){
+    cor(cluster_res_varimp_list[[x]][["rank"]], cluster_res_varimp_list[[cluster_btw]][["rank"]], method = c("spearman"))
+  }))
+  act_res <- as.data.frame(cbind(act_clust_2, act_cor))
+  colnames(act_clust_2) <- c("cluster", "correlation")
+  act_res$correlation <- as.numeric(act_res$correlation)
+  return(act_res)
+}
+
+rank_btw_corr_res <- lapply(c(1:max(clust_dat$groups)-1), rc_btw_cluster)
+
+
+#Manually
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[2]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[3]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[4]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[5]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[6]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[1]][["rank"]], cluster_res_varimp_list[[7]][["rank"]], method = c("spearman"))
+
+cor(cluster_res_varimp_list[[2]][["rank"]], cluster_res_varimp_list[[3]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[2]][["rank"]], cluster_res_varimp_list[[4]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[2]][["rank"]], cluster_res_varimp_list[[5]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[2]][["rank"]], cluster_res_varimp_list[[6]][["rank"]], method = c("spearman"))
+cor(cluster_res_varimp_list[[2]][["rank"]], cluster_res_varimp_list[[7]][["rank"]], method = c("spearman"))
+#etc
+
+#Repeated measures ANOVA
+
+#get data into required format
+rma_cluster_res_varimp <- cluster_res_varimp[with(cluster_res_varimp, order(groups, feature)),]
+
+rma_cluster_res_varimp$groups <- as.factor(rma_cluster_res_varimp$groups)
+rma_cluster_res_varimp$feature <- as.factor(rma_cluster_res_varimp$feature)
+
+#Summary
+rma_cluster_res_varimp %>%
+  group_by(groups) %>%
+  get_summary_stats(importance, type ="mean_sd")
+
+#ANOVA assumptions
+
+
+##Normality
+
+rma_cluster_res_varimp %>%
+  group_by(groups) %>%
+  shapiro_test(importance) #normality fails
+
+ggqqplot(rma_cluster_res_varimp, "importance", facet.by = "groups") #normality fails -> non parametric test needed
+
+#Friedman-test
+
+res_fried <-rma_cluster_res_varimp %>%
+  friedman_test(importance ~ groups | feature )
+res_fried #groups are significantly different
+
+##Effect size
+rma_cluster_res_varimp %>% friedman_effsize(importance ~ groups | feature) #small effect size :/
+
+##Multiple pairwise-comparisons (Wilcoxon signed-rank test)
+# pairwise comparisons
+pwc <- rma_cluster_res_varimp %>%
+  wilcox_test(importance ~ groups, paired = TRUE, p.adjust.method = "bonferroni")
+pwc
+print(tbl_df(pwc), n=40) #clusters significatly differ (5 pairs out of the 21 combinations): 
+#                                                        1 - 4, 1- 6, 1 - 7
+#                                                        4 -5, 5 -6
+
+
+
+
